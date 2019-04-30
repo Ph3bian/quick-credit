9f017f242c90e4ff3aeaff9e7a01c4229e2e4860
Phebian Chukwurah <phebianchukwurah@gmail.com>
2019-04-30 12:43:10 +0100

    feature(api): User sign in [Finishes #165478305]

--- server/memory/user.js
+++ server/memory/user.js
@@ -1,1 +1,19 @@
-export default [];
+export default [
+  {
+    message: 'Great! Sign up successful',
+    data: {
+      id: '571878',
+      status: 'unverified',
+      user: {
+        firstname: 'Samantha',
+        lastname: 'J',
+        email: 'hello@gmail.com',
+        password: '$2a$10$OOdD4PC4cSjk8pmRM.Wzgux8MLrgilxg3XMpmH80h4zkb/MOrE.gK',
+        address: 'ikeja Gra',
+        bvn: '22307821876',
+      },
+      token: 'Bearer 1234567899999888',
+      isAdmin: false,
+    },
+  },
+];
--- server/validation/validator.js
+++ server/validation/validator.js
@@ -4,11 +4,15 @@ export default class Validator {
     req.checkBody('firstname', 'First name must be alphabets').isAlpha();
     req.checkBody('lastname', 'Last name is required').notEmpty();
     req.checkBody('lastname', 'Last name Must be alphabets').isAlpha();
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('email', 'Valid email is required').notEmpty().isEmail();
     req.checkBody('password', 'Password is required').notEmpty();
     req.checkBody('address', 'Address is required').notEmpty();
     req.checkBody('bvn', 'BVN is required').notEmpty();
     req.checkBody('bvn', 'BVN must be 11 characters').isLength({ min: 11, max: 11 });
+  }
+
+  static userSignIn(req) {
+    req.checkBody('email', 'Valid email is required').notEmpty().isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
   }
 }
