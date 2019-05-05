47ad482122caaea511f7dc51a3af6769d5f7fdf9
Phebian Chukwurah <phebianchukwurah@gmail.com>
2019-05-05 03:41:03 +0100

    feature(api): Built out endpoint to request for loan,
    Updated styling for loan request view
    
    Updated general styling
    
    [Finishes #165592022, #165809436]

--- server/validation/validator.js
+++ server/validation/validator.js
@@ -15,4 +15,11 @@ export default class Validator {
     req.checkBody('email', 'Valid email is required').notEmpty().isEmail();
     req.checkBody('password', 'Password is required').notEmpty();
   }
+
+  static requestLoan(req) {
+    req.checkBody('amount', 'Amount is required').notEmpty();
+    req.checkBody('tenor', 'tenor is required').notEmpty();
+    req.checkBody('loanType', 'tenor is required').notEmpty();
+    req.checkBody('accountNo', 'tenor is required').notEmpty();
+  }
 }
