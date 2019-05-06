83f35d23c0aaeae88b8c06d59cb389ae58955283
Phebian Chukwurah <phebianchukwurah@gmail.com>
2019-05-06 02:39:44 +0100

    feature(api): Built out Admin get all loan applications Included morgan Updated styling in home page on mobile view Updated babel
    [Finishes #165655259]

---
+++ server/memory/loans.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 983415,
+    createdOn: '2019-05-06T00:10:04.538Z',
+    balance: 4200,
+    paymentInstallment: 700,
+    status: 'pending',
+    amount: 4000,
+    tenor: 6,
+    loanType: 'SF',
+    repaid: false,
+    accountNo: '2048801364',
+  },
+];
