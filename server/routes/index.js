c6e47962a7a06417aef671fbcca6dd4387608a70
Phebian Chukwurah <phebianchukwurah@gmail.com>
2019-05-06 04:17:10 +0100

    feature(api): Built out endpoint to mark user as verified
    PATCH /users/<:user-email>/verify
    
    also updated response from sign up and sign in to match API Endpoint Specification
    
    [Finishes #165655177]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import AuthController from '../controllers/authController';
 import LoanController from '../controllers/loanController';
+import UserController from '../controllers/userController';
 
 const router = new Router();
 router.post('/signup', AuthController.signUp);
 router.post('/signin', AuthController.signIn);
 router.post('/loans', LoanController.requestLoan);
 router.get('/loans', LoanController.fetchLoans);
+router.patch('/users/:email/verify', UserController.verifyUser);
 export default router;
