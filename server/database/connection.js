ee76defa6052cc7dd0d698af9e7bd2bb6f345011
Phebian Chukwurah <user@Phebian.local>
2019-05-17 00:49:05 +0100

    updated travis file

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,11 +2,11 @@
 import { Client } from 'pg';
 
 const client = new Client({
-  user: 'user',
-  password: '',
-  port: 5432,
-  database: 'postgres',
-  host: 'localhost',
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || '',
+  port: process.env.DB_PORT ||  5432,
+  database: process.env.DB_DATABASE || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
 });
 
 client.connect();
